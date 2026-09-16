793e8beef219e1086ce111bf9b62326c09e911dc
emmanuel776666 <emmanuelchibuike482@gmail.com>
2026-09-16 14:08:00 +0100

    Update cron schedule to run morning news bot at 7 AM Nigeria time

--- index.js
+++ index.js
@@ -310,8 +310,8 @@ async function runBot() {
 
 
 
-cron.schedule("0 9 * * *", () => {
-  console.log("9:00 AM Nigeria time. Starting morning news bot...");
+cron.schedule("0 7 * * *", () => {
+  console.log("7:00 AM Nigeria time. Starting morning news bot...");
   runBot();
 }, {
   timezone: "Africa/Lagos"
